5f98b938d34208e801296a6c6123f8c277b276ac
John Johnson <dampboxdev@gmail.com>
2013-10-08 18:11:51 -0700

    Installed Updated Bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProtoBlog::Application.config.secret_key_base = '275cf53f802b5348166ae38c149f021b0bf92863a516ea23bcefd6db97900b201b0ab5b18ad974a458a94d194980bcdec569594bbf5c240234ebe56b9c9f7162'
