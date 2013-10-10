c7d82cbd54b9f2eda78d4f43718d0bbefe4ef081
John Johnson <dampboxdev@gmail.com>
2013-10-09 18:14:25 -0700

    Posts Complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,40 @@
 class PostsController < ApplicationController
 
 	def new
+		@post = Post.new
 	end
 
 	def create
-		render text: params[:post].inspect
+		@post = Post.new(params[:post].permit(:title, :text))
+
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
 	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def index
+		@posts = Post.all
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to posts_path
+	end
+
+	private
+		def post_params
+			params.require(:post).permit(:title, :text)
+		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ ProtoBlog::Application.routes.draw do
   resources :posts
 
   root to:"welcome#index"
-
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
